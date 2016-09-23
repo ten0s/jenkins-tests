9463375e83666f5ff103472f775080794e758465
Dmitry Klionsky <dm.klionsky@gmail.com>
2016-09-23 15:07:51 +0300

    WIP

--- ecr-email.groovy
+++ ecr-email.groovy
@@ -1,41 +1,11 @@
 #!groovy
 
-class ECRImage implements Serializable {
-    def image
-    def registryId
-    def region
-
-    ECRImage(image) {
-        this.image = image
-
-        def parsed = image.tokenize('.')
-        this.registryId = parsed[0]
-        this.region = parsed[3]
-    }
-
-    // https://support.cloudbees.com/hc/en-us/articles/217736618-How-do-I-access-Pipeline-DSLs-from-inside-a-Groovy-class
-    def pull(script) {
-      script.sh "aws ecr get-login --region ${region} --registry-ids ${registryId} | sh"
-      script.sh "docker pull ${image}"
-    }
-
-    def inside(script, closure) {
-        pull(script)
-
-        // create tmp dir and return its name
-        def out_dir = script.sh(returnStdout: true, script: 'mktemp -d -p $(pwd)').trim()
-
-        def container = script.docker.image(image)
-        container.inside("-u root -v ${out_dir}:/out/") {
-            closure('/out/')
-        }
-
-        return out_dir
-    }
-}
-
 node('dev_linux_awscli_docker') {
     try {
+        def ecrImage = load("ecr-image.groovy")
+        ecrImage.test("print me")
+
+        /*
         def image = new ECRImage("312226949769.dkr.ecr.us-east-1.amazonaws.com/centos7.x86_64:latest")
         def out_dir = image.inside(this) { out_dir ->
             sh "date > ${out_dir}/test"
@@ -43,6 +13,7 @@ node('dev_linux_awscli_docker') {
 
         sh "cat ${out_dir}/test"
         sh "aws s3 cp ${out_dir}/test s3://idtq-deployment-jenkins-hermes/"
+        */
     }
     catch (e) {
         currentBuild.result = 'FAILURE'
