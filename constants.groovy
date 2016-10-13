cb75a120360e15745d049bea5010e511d83c0e0d
Dmitry Klionsky <dm.klionsky@gmail.com>
2016-10-13 18:43:34 +0300

    WIP

--- constants.groovy
+++ constants.groovy
@@ -2,11 +2,11 @@
 
 // git url: 'https://github.com/ten0s/jenkins-tests.git', credentialsId: '5948caec-9a2f-4a0b-a07c-3eb562985758'
 // def c = load('constants.groovy')
-// def consts = c.getConsts()
-// echo consts
+// def recipients = c.getRecipients()
+// echo recipients
 
-def getConsts() {
-  return 'abc;def'
+def getRecipients() {
+  return 'recipient1;recipient2'
 }
 
 return this
