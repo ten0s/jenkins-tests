e897f9aa112743d1d66f62b27c226790b1c8b56d
Dmitry Klionsky <dm.klionsky@gmail.com>
2016-10-14 11:44:37 +0300

    Add comments

--- constants.groovy
+++ constants.groovy
@@ -1,7 +1,24 @@
 #!groovy
 
-// git url: 'https://github.com/ten0s/jenkins-tests.git', credentialsId: '5948caec-9a2f-4a0b-a07c-3eb562985758'
+// def repoUrl = 'https://github.com/ten0s/jenkins-tests.git'
+// def credId = '5948caec-9a2f-4a0b-a07c-3eb562985758' or null
+// def nodeLabel = 'qa_linux_awscli_docker' // if '' call hangs
+//
+// def c;
+//
+// git url: repoUrl, credentialsId: credId
 // def c = load('constants.groovy')
+//
+// OR
+//
+// c = fileLoader.fromGit('constants.groovy', repoUrl, 'master', credId, nodeLabel)
+//
+// OR
+//
+// fileLoader.withGit(repoUrl, 'master', credId, nodeLabel) {
+//   c = load('constants.groovy')
+// }
+//
 // def recipients = c.getRecipients()
 // echo recipients
 
